c9845867050b7b18e15723e75f4615abe3c7a3a1
Tina <box2298@gmail.com>
2022-02-16 16:37:23 +0800

    0216 lesson

---
+++ routes/ws-echo.js
@@ -0,0 +1,17 @@
+const WebSocket = require('ws'); 
+const createEchoServer = server=>{
+    const wsServer = new WebSocket.Server({server});  
+
+    wsServer.on('connection', (ws,req)=>{
+        console.log('連線數:' ,wsServer.clients.size);
+        ws.on('message', message=>{
+            ws.send(message);
+        });
+        ws.send('hi');
+});
+
+
+
+};
+
+module.exports = createEchoServer;
